41ec0bd54c88c057fb4801ab986b3529e8d5a08f
Treveshan Naidoo <treveshan.naidoo@chillisoft.co.za>
2017-03-18 01:09:43 +0200

    first commit

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string message, string color ,string user)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.UpdateChatMessage(message, color,user);
+        }
+    }
+}
